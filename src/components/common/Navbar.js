c5d65f548b8ef163b71333fc8aca758aa69465a9
RHThree <russelheiser@gmail.com>
2025-10-15 23:03:55 -0400

    header current page bolding and recolor buy now

--- src/components/common/Navbar.js
+++ src/components/common/Navbar.js
@@ -1,6 +1,8 @@
-import { Link } from 'react-router-dom';
+import { Link, useLocation } from 'react-router-dom';
 
 const Navbar = () => {
+	const location = useLocation();
+
 	return (
 		<nav className="bg-primary-bg text-primary-text p-4 shadow">
 			<div className="container mx-auto flex justify-between items-center">
@@ -8,16 +10,28 @@ const Navbar = () => {
 					PitStopProvisions
 				</Link>
 				<div className="flex space-x-4">
-					<Link to="/purchase" className="hover:underline font-semibold">
+					<Link
+						to="/purchase"
+						className={`hover:underline font-semibold text-primary-bg-hover ${location.pathname === '/purchase' ? 'font-extrabold' : ''}`}
+					>
 						Buy Now
 					</Link>
-					<Link to="/products" className="hover:underline">
+					<Link
+						to="/products"
+						className={`hover:underline ${location.pathname === '/products' ? 'font-bold' : ''}`}
+					>
 						Products
 					</Link>
-					<Link to="/about-us" className="hover:underline">
+					<Link
+						to="/about-us"
+						className={`hover:underline ${location.pathname === '/about-us' ? 'font-bold' : ''}`}
+					>
 						About Us
 					</Link>
-					<Link to="/contact-us" className="hover:underline">
+					<Link
+						to="/contact-us"
+						className={`hover:underline ${location.pathname === '/contact-us' ? 'font-bold' : ''}`}
+					>
 						Contact Us
 					</Link>
 				</div>
